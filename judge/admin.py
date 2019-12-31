bbcc603df015229b09843cb343dc6f95301d337d
Paul Lee <enigma@enigmatic.network>
2019-12-30 23:53:43 -0500

    start over

--- judge/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Problem, Testcase
-
-# Register your models here.
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Problem)
-admin.site.register(Testcase)
