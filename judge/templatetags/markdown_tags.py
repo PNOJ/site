fc5a851ae5b7ff747108ab5ed7eab2993630029e
Paul Lee <enigma@enigmatic.network>
2020-03-09 17:23:36 -0400

    change markdown engine and add LaTeX support

--- judge/templatetags/markdown_tags.py
+++ judge/templatetags/markdown_tags.py
@@ -1,14 +1,19 @@
 import requests
+import markdown as markdown_pkg
+
 from django import template
 from django.utils.safestring import mark_safe
  
 register = template.Library()
  
-# return an image tag with the gravatar
-# TEMPLATE USE:  {{ email|gravatar:150 }}
+# @register.filter
+# def markdown(markdown_content, mode="gfm"):
+#     post_content = {'text': markdown_content, 'mode': mode, 'context': 'pnoj/site'}
+#     rendered_markdown = requests.post("https://api.github.com/markdown", json=post_content)
+#     rendered_markdown.raise_for_status()
+#     return mark_safe(rendered_markdown.text)
+
 @register.filter
-def markdown(markdown_content, mode="gfm"):
-    post_content = {'text': markdown_content, 'mode': mode, 'context': 'pnoj/site'}
-    rendered_markdown = requests.post("https://api.github.com/markdown", json=post_content)
-    rendered_markdown.raise_for_status()
-    return mark_safe(rendered_markdown.text)
+def markdown(markdown_content):
+    html = markdown_pkg.markdown(markdown_content)
+    return mark_safe(html)
