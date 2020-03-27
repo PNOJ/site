7eb1a4be75685fddb0759bd8012727920b8674d6
Paul Lee <enigma@enigmatic.network>
2020-03-27 13:20:02 -0400

    add submission_source view

--- judge/views/submission.py
+++ judge/views/submission.py
@@ -2,6 +2,7 @@ from django.views.generic import DetailView, ListView
 from .. import models
 from django.views.generic.base import RedirectView
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 class SubmissionIndex(ListView):
     model = models.Submission
@@ -32,4 +33,21 @@ class Submission(DetailView):
         context['page_title'] = 'PNOJ: Submission #' + str(self.get_object().pk)
         submission_contenttype = ContentType.objects.get_for_model(models.Submission)
         context['comments'] = models.Comment.objects.filter(parent_content_type=submission_contenttype, parent_object_id=self.get_object().pk)
+        return context
+
+class SubmissionSource(UserPassesTestMixin, DetailView):
+    model = models.Submission
+    context_object_name = 'submission'
+    template_name = 'judge/submission_source.html'
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sidebar_items'] = models.SidebarItem.objects.order_by('order')
+        context['page_title'] = 'PNOJ: Submission #' + str(self.get_object().pk)
+        submission_contenttype = ContentType.objects.get_for_model(models.Submission)
+        with self.get_object().source.open() as sourcefile:
+            context['source'] = sourcefile.read().decode('ascii').strip()
         return context
